c3a913d838b5cb394d2af6a8e1966276c32f0929
Jiri Sembera <jirisembera@gmail.com>
2017-02-12 14:03:59 +0100

    Removed unreliable node.input() from remote_console (did not work with a device connected to UART)

---
+++ lua/buffered_socket.lua
@@ -0,0 +1,83 @@
+-- buffered_socket.lua - net.socket wrapper that allows comfortably call send() without having to deal with
+--   "sent" callbacks (cannot send another message unless the previous has been sent...)
+
+
+BufferedSocket = {}
+BufferedSocket.__index = BufferedSocket
+
+-- allow construction via directly calling the meta-table
+setmetatable(BufferedSocket, {
+  __call = function (cls, ...)
+    return cls.new(...)
+  end,
+})
+
+local socket_cache = {} -- BufferedSocket cache
+
+-- actual constructor
+function BufferedSocket.new(socket)
+    if socket_cache[socket] ~= nil then
+        return socket_cache[socket]
+    end
+
+    local self = setmetatable({}, BufferedSocket)
+    
+    self._socket = socket
+    self._queue = {} -- messages to send
+    self._send_pending = false -- a message is currently being sent
+    self._all_sent_callback = nil -- callback fired when self._queue is empty
+    self._disconnection_callback = nil -- on disconnect callback
+
+    socket:on("disconnection", function( sck, error_code )
+        socket_cache[sck] = nil
+        if self._disconnection_callback then
+            self._disconnection_callback(self, error_code)
+        end
+    end)
+
+    socket_cache[socket] = self -- store in cache
+    return self
+end
+
+function BufferedSocket:send(data)
+    if self._send_pending then
+        table.insert(self._queue, 1, data) -- enqueue
+    else
+        self._send_pending = true
+        self._socket:send(data, function(sck)
+            if #self._queue > 0 then  -- send next message
+                local data = table.remove(self._queue) 
+                sck:send(data)
+            else -- all sent
+                self._send_pending = false
+                if self._all_sent_callback then
+                    self._all_sent_callback(self) -- notify everything has been sent
+                end
+            end
+        end)
+    end
+end
+
+function BufferedSocket:on(event, callback)
+    if event == "sent" then -- capture sent event - fire when everything has been sent
+        self._all_sent_callback = callback
+    elseif event == "disconnection" then
+        self._disconnection_callback = callback
+    else -- pass other events to underlying socket
+        self._socket:on(event, function(sck, arg2)
+            callback(BufferedSocket(sck), arg2)
+        end)
+    end
+end
+
+-- Returns socket passed in constructor
+function BufferedSocket:get_socket()
+    return self._socket
+end
+
+-- Returns socket passed in constructor
+function BufferedSocket:close()
+    return self._socket:close()
+end
+
+
--- lua/remote_console.lua
+++ lua/remote_console.lua
@@ -1,21 +1,18 @@
 -- TCP/IP remote console. Allows to interact with NodeMCU over wifi in a similar way as over serial port.
 -- Don't forget to send AUTH_MAGIC after connecting. Tested with Raw socket connection via PuTTY.
+--
+-- Known issues: Prompt character (">") gets sometimes sent twice (at least with Putty client).
+--               It is caused by two on_receive events, one for entered string in console, second for \r\n
 -- 
--- required modules: net, node
+-- required modules: net, node, buffered_socket.lua
 
 -- CONFIG
 local LISTEN_PORT = 65432
 local CONNECTION_TIMEOUT = 1200 -- 20min
 local AUTH_MAGIC = "<secret>" -- have to send 
-local EVAL_INPUT = true --use node.input() instead of loadstring()
-                        --  node.input() is closer to serial console but does not allow to report errors
 
 -- LOCALS
 local authorized = {}
-
-function _node_input(val)
-    EVAL_INPUT = val
-end
 
 -- event handlers
 function send_output(str)
@@ -32,20 +29,18 @@ function on_receive(sck, data)
         if data:sub(1, AUTH_MAGIC:len()) == AUTH_MAGIC then
             authorized[sck] = 1
             node.output(send_output, false) -- write result to both socket and serial
-            sck:send("Welcome! Use _node_input(false) to switch to loadstring() evaluation. _node_input(true) switches back to node.input() evaluation (default)")
+            sck:send("Welcome!\r\n> ")
         end
         return
     end
 
-    -- process data
-    if EVAL_INPUT then
-        node.input(data)
-    else
-        local success, error = pcall(loadstring(data))--pcall(node.input, data)
-        if not success then
-            sck:send(error)
-        end
+    -- process command
+    local success, error = pcall(loadstring(data))--pcall(node.input, data)
+    if not success then
+        sck:send(error)
     end
+    
+    sck:send("\r\n> ")
 end
 
 function on_close(sck)
@@ -58,7 +53,9 @@ remote_console = net.createServer(net.TCP, CONNECTION_TIMEOUT)
 
 if remote_console then
     remote_console:listen(LISTEN_PORT, function(conn)
-        conn:on("receive", on_receive)
-        conn:on("disconnection", on_close)
+        buff_conn = BufferedSocket(conn)
+        
+        buff_conn:on("receive", on_receive)
+        buff_conn:on("disconnection", on_close)
     end)
 end
