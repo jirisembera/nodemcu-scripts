2af7109fd5371fdfb31b0305fd8cff79f14bd9af
Jiří Šembera <jirisembera@gmail.com>
2020-12-19 15:01:43 +0100

    Ditched PRODUCTION variable, always pre-compile sources.

--- lua/init_autocompile.lua
+++ lua/init_autocompile.lua
@@ -1,33 +1,30 @@
 --
--- Sample init.lua with auto-compile: Develop using .lua sources, run compiled bytecode (.lc) in production
--- 
--- Usage:
---   * List files to be executed in the FILES table !! without extension !!
---   * Set PRODUCTION = false for development
---   * Change to PRODUCTION = true for production deployment. ".lc" files get auto-recompiled on the first production run and then re-used
+-- Safe init.lua with auto-compile
 --
 
 FILES = {"buffered_socket", "remote_console"}
 PRODUCTION = false
 
 for i = 1,#FILES do
-    local file_to_execute = nil
+    local file_to_execute = FILES[i] .. ".lc"
+    local file_to_compile = FILES[i] .. ".lua"
+    local file_backup = FILES[i] .. ".lua.bak"
 
-    if PRODUCTION then
-        file_to_execute = FILES[i] .. ".lc"
-        if not file.exists( file_to_execute ) then
-            node.compile( FILES[i] .. ".lua" )
-        end
-    else
-        file_to_execute = FILES[i] .. ".lua"
-        local file_to_remove = FILES[i] .. ".lc"
-        if file.exists( file_to_remove ) then
-            file.remove( file_to_remove )
+    if file.exists( file_to_compile ) then
+        print("Compiling " .. file_to_compile)
+        local success, err_msg = pcall(node.compile, file_to_compile)
+        if success then
+            file.remove(file_backup)
+            file.rename(file_to_compile, file_backup)
+        else
+            print("Failed to compile " .. file_to_compile .. " error: " .. err_msg)
         end
     end
-    
+
+    print("Loading " .. file_to_execute)
     local success, err_msg = pcall(dofile, file_to_execute)
     if not success then
         print("Failed to load " .. file_to_execute .. " error: " .. err_msg)
     end
+    collectgarbage()
 end
